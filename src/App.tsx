b617710a229f5b8e71f99a0d9bd7d336d16b9c25
Davi <davihga190@gmail.com>
2025-10-04 00:10:16 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Header } from "./components/header";
+import { Footer } from "./components/footer";
+import style from "./App.module.css";
+import logo from "./imgs/logoMedCenter.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.body}>
+      <Header />
+      <main>
+        <div className={style.pagina}>
+          <div className={style.info}>
+            <div className={style.logo}>
+              <img src={logo} alt="" />
+              <h1>MedCenter</h1>
+            </div>
+            <p>Agendamento clinico</p>
+          </div>
+          <div className={style.cadastro}>
+            <h3>Cadastro</h3>
+            <p>Escolha um campo a baixo</p>
+            <div className={style.opcoes}>
+              <span>Ususario</span>
+              <span>Medico</span>
+            </div>
+            <div className={style.campos}>
+              <div className={style.linha}>
+                <label htmlFor="">nome Completo</label>
+                <input type="text" placeholder="Digite seu nome completo" />
+              </div>
+              <div className={style.coluna}>
+                <div className={style.linha}>
+                  <label htmlFor="">CPF</label>
+                  <input type="number" placeholder="000.000.000-00" />
+                </div>
+                <div className={style.linha}>
+                  <label htmlFor="">Data de Nascimento</label>
+                  <input type="date" />
+                </div>
+              </div>
+              <div className={style.linha}>
+                <label htmlFor="">Email</label>
+                <input type="email" placeholder="seu@email.com" />
+              </div>
+              <div className={style.coluna}>
+                <div className={style.linha}>
+                  <label htmlFor="">telefone</label>
+                  <input type="number" placeholder="(00)00000-000" />
+                </div>
+                <div className={style.groupForm}>
+                  <label htmlFor="">Sexo</label>
+                  <select required>
+                    <option value="">Selecione</option>
+                    <option value="M">Masculino</option>
+                    <option value="F">Feminino</option>
+                    <option value="O">outro</option>
+                  </select>
+                </div>
+              </div>
+              <div className={style.linha}>
+                <label htmlFor="">Senha</label>
+                <input type="password" placeholder="minimo 8 caracteres" />
+              </div>
+              <button className = {style.btn}>Cadastrar Conta</button>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
-
 export default App;
---
+++ src/components/header/index.tsx
@@ -0,0 +1,34 @@
+import medCenter from "../../imgs/logoMedCenter.png";
+import style from "./header.module.css";
+
+export const Header = () => {
+  return (
+    <header className={style.header}>
+      <div className={style.logo}>
+        <img src={medCenter} alt="Logo MedCenter" />
+        <h1>MedCenter</h1>
+      </div>
+      <nav>
+        <ul className={style.info}>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li className={style.dropdown}>
+            <a href="#">Serviços</a>
+            <ul className={style.services}>
+              <li>
+                <a href="#">Cadastro</a>
+              </li>
+              <li>
+                <a href="#">Agendamento</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Sobre Nós</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
